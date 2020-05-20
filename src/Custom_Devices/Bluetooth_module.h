443386af69600c2ac2c81fba075aee8bb5f6f717
Raphael <benistan@etud.insa-toulouse.fr>
2020-05-20 10:03:32 +0200

    Started Bluetooth device

---
+++ src/Custom_Devices/Bluetooth_module.h
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Bluetooth_module.h
+ * Author: raphael
+ *
+ * Created on May 20, 2020, 9:58 AM
+ */
+
+#ifndef BLUETOOTH_MODULE_H
+#define BLUETOOTH_MODULE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern bool connection_request_bluetooth;
+extern bool connected_bluetooth; //Bluetooth is connected
+
+
+#ifdef __cplusplus
+}
+#endif
+
+// exemple d'actionneur sur le bus UART permettant d'echanger des tableaux de caracteres : un ecran, ne pas oublier d'heriter de Device
+class UARTActuatorBluetoothModule : public Device{
+protected:
+    // memorise les données envoyées au module WIFI
+  char buf[UART_BUFFER_SIZE];
+  
+public:
+  // constructeur
+  UARTActuatorBluetoothModule ();
+  //Connection du Module Bluetooth à un appareil Bluetooth
+  void connect();
+  // thread representant le capteur et permettant de fonctionner independamment de la board
+  virtual void run();
+};
+
+#endif /* BLUETOOTH_MODULE_H */
+
