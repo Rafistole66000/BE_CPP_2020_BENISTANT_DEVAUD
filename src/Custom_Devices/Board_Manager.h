8f8acf24660f06a7b1fa7c1a65308197b8c045ba
Raphael <benistan@etud.insa-toulouse.fr>
2020-05-23 09:30:12 +0200

    Makefile modifié par rapport à la remarque du prof sur GitHub

---
+++ src/Custom_Devices/Board_Manager.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Board_Manager.h
+ * Author: raphael
+ *
+ * Created on May 23, 2020, 9:27 AM
+ */
+
+#ifndef BOARD_MANAGER_H
+#define BOARD_MANAGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_MANAGER_H */
+
